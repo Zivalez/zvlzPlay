7cf18240f9000ebe88fd72fc807a80eb7da25721
Zivalez <adit050604@gmail.com>
2026-02-19 19:47:10 +0700

    fix nontondrama list

--- NontonDrama/src/main/kotlin/com/nontondrama/NontonDrama.kt
+++ NontonDrama/src/main/kotlin/com/nontondrama/NontonDrama.kt
@@ -51,25 +51,22 @@ class NontonDrama : MainAPI() {
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data + page).document
-        val home = document.select("article.mega-item, article.item, article.post, div.item, div.movie, div.result-item, div.col-md-2").mapNotNull { it.toSearchResult() }
+        val home = document.select("li.slider article, article.item, article.post, div.item, div.movie, div.col-md-2")
+                .mapNotNull { it.toSearchResult() }
         return newHomePageResponse(request.name, home)
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("h1.grid-title > a, h2.grid-title > a, h3 > a, .title > a")?.ownText()?.trim()
-                ?: this.selectFirst("h1, h2, h3, .title")?.text()?.trim()
+        val title = this.selectFirst(".poster-title, h3.poster-title, .title, h1, h2, h3")?.text()?.trim()
                 ?: this.selectFirst("a")?.attr("title")
                 ?: return null
         val href = fixUrl(this.selectFirst("a")!!.attr("href"))
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
         
         // Ensure strictly series/drama
-        return if (this.selectFirst("div.last-episode") != null) {
-            val episode =
-                    this.selectFirst("div.last-episode span")
-                            ?.text()
-                            ?.filter { it.isDigit() }
-                            ?.toIntOrNull()
+        val episodeElement = this.selectFirst("span.episode, div.last-episode")
+        return if (episodeElement != null) {
+            val episode = episodeElement.text().filter { it.isDigit() }.toIntOrNull()
             newAnimeSearchResponse(title, href, TvType.TvSeries) {
                 this.posterUrl = posterUrl
                 addSub(episode)
