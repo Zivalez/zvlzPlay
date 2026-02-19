20fc7980a6946a5ac1ada68c037cb9194c273201
Zivalez <adit050604@gmail.com>
2026-02-19 20:02:57 +0700

    add samehadaku

---
+++ Samehadaku/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    namespace = "com.samehadaku"
+    
+    defaultConfig {
+        minSdk = 21
+        compileSdkVersion(35)
+        targetSdk = 35
+    }
+}
+
+dependencies {
+    implementation("com.lagradost:cloudstream3:pre-release")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.json:json:20240303")
+}
+version = 1
+
+cloudstream {
+    language = "id"
+    description = "Anime Sub Indo"
+    authors = listOf("Zivalez", "Asm0d3usX")
+    status = 3
+    tvTypes = listOf(
+        "AnimeMovie",
+        "OVA",
+        "Anime",
+    )
+    iconUrl = "https://v1.samehadaku.how/wp-content/uploads/2020/04/cropped-download-1-32x32.jpg"
+}
