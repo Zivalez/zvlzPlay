f820cabaa525aaa5e733c97342b8a6e943a8fe5a
Zivalez <adit050604@gmail.com>
2026-02-21 22:28:19 +0700

    add otakudesu

---
+++ Otakudesu/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 3
+
+cloudstream {
+    language = "id"
+    description = "Streaming Anime Sub Indo"
+    authors = listOf("Zivalez")
+    isCrossPlatform = true
+    status = 3
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
+    iconUrl = "https://otakudesu.best/wp-content/uploads/2019/08/otakudesu.png"
+}
