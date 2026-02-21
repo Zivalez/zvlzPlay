5f93340c1ac1f7b7eb40c07827993c56803b04d3
Zivalez <adit050604@gmail.com>
2026-02-21 22:50:23 +0700

    fix otakudesu

--- Otakudesu/src/main/kotlin/com/otakudesu/Otakudesu.kt
+++ Otakudesu/src/main/kotlin/com/otakudesu/Otakudesu.kt
@@ -11,7 +11,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
-import kotlinx.coroutines.runBlocking
+
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
@@ -188,21 +188,19 @@ class Otakudesu : MainAPI() {
         quality: Int = Qualities.Unknown.value
     ) {
         loadExtractor(url, referer, subtitleCallback) { link ->
-            runBlocking {
-                callback.invoke(
-                    newExtractorLink(
-                        link.name,
-                        link.name,
-                        link.url,
-                        link.type
-                    ) {
-                        this.referer = link.referer
-                        this.quality = quality
-                        this.headers = link.headers
-                        this.extractorData = link.extractorData
-                    }
-                )
-            }
+            callback.invoke(
+                newExtractorLink(
+                    link.name,
+                    link.name,
+                    link.url,
+                    link.type
+                ) {
+                    this.referer = link.referer
+                    this.quality = quality
+                    this.headers = link.headers
+                    this.extractorData = link.extractorData
+                }
+            )
         }
     }
 
