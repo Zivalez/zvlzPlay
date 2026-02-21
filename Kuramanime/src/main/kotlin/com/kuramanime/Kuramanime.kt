6630b6aaf3887ea40ecab77a8826d7ae18c7c5bf
Zivalez <adit050604@gmail.com>
2026-02-21 22:07:12 +0700

    fix kuramanime

--- Kuramanime/src/main/kotlin/com/kuramanime/Kuramanime.kt
+++ Kuramanime/src/main/kotlin/com/kuramanime/Kuramanime.kt
@@ -8,7 +8,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class Kuramanime : MainAPI() {
-    override var mainUrl = "https://v14.kuramanime.tel"
+    override var mainUrl = "https://v15.kuramanime.tel"
     override var name = "Kuramanime"
     override val hasQuickSearch = false
     override val hasMainPage = true
