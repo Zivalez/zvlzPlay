b59dc72512edf0762df637179d94e9206c788e30
Zivalez <adit050604@gmail.com>
2026-02-22 20:41:48 +0700

    fix pencurimovie

--- Pencurimovie/build.gradle.kts
+++ Pencurimovie/build.gradle.kts
@@ -1,11 +1,11 @@
-version = 1
+version = 3
 
 cloudstream {
-    description = "Pencurimovie"
+    description = "Nonton Film & Serial Streaming Online"
     language = "id"
     authors = listOf("Phisher98", "Zivalez")
     status = 1
-    tvTypes = listOf("Movie", "TvSeries")
+    tvTypes = listOf("Movie")
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ww03.pencurimovie.bond&size=%size%"
     isCrossPlatform = true
 }
