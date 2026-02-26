f551868bbfda9255b423e22fed9a4388d970deb8
Zivalez <adit050604@gmail.com>
2026-02-26 20:10:15 +0700

    add provider moviebox

---
+++ Moviebox/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    description = "Movie Subtitle Indonesia"
+    authors = listOf("Zivalez")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama",
+    )
+    
+    iconUrl = "https://cloudstream.zvlz.my.id/icon/moviebox.png"
+}
