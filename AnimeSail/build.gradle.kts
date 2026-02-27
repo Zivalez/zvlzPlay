9d4c738d8f2014b3495024b4fecad7a3de264375
Zivalez <adit050604@gmail.com>
2026-02-28 01:19:21 +0700

    add new provider animesail

---
+++ AnimeSail/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    authors = listOf("Asm0d3usX", "Zivalez")
+    description = "Streaming Anime Subtitle Indonesia dari AnimeSail"
+    isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA"
+    )
+    iconUrl = "https://aghanim.xyz/wp-content/themes/animesail/assets/images/ico.png"
+}
