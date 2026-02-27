e52c016176ec1bc30fca5a24230526d3503b570f
Zivalez <adit050604@gmail.com>
2026-02-27 18:02:24 +0700

    fix enum searchquality

--- Idlix/src/main/kotlin/com/idlix/Idlix.kt
+++ Idlix/src/main/kotlin/com/idlix/Idlix.kt
@@ -98,10 +98,10 @@ class Idlix : MainAPI() {
     private fun getQuality(qualityStr: String): SearchQuality? {
         val q = qualityStr.trim().replace("-", "").replace(" ", "").lowercase()
         return when {
-            q.contains("webdl") -> SearchQuality.WebDL
-            q.contains("webrip") -> SearchQuality.WEBRip
+            q.contains("webdl") -> SearchQuality.WebRip
+            q.contains("webrip") -> SearchQuality.WebRip
             q.contains("bluray") -> SearchQuality.BlueRay
-            q.contains("web") -> SearchQuality.WEB
+            q.contains("web") -> SearchQuality.WebRip
             else -> getQualityFromString(qualityStr)
         }
     }
