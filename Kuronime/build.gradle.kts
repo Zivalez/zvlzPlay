2a22dda6de4ad8f88e2d35f676fa1444ae061f60
Zivalez <adit050604@gmail.com>
2026-04-16 22:35:22 +0700

    fix name extractor kuronime build 5

--- Kuronime/build.gradle.kts
+++ Kuronime/build.gradle.kts
@@ -1,6 +1,12 @@
 // use an integer for version numbers
 version = 4
 
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.json:json:20240303")
+}
 
 cloudstream {
     language = "id"
