7f3615eb3ba88f994a075a6268236c598171b5c3
Zivalez <adit050604@gmail.com>
2026-04-16 21:51:07 +0700

    fix name extractor kuronime build

--- Kuronime/src/main/kotlin/com/kuronime/Kuronime.kt
+++ Kuronime/src/main/kotlin/com/kuronime/Kuronime.kt
@@ -9,6 +9,7 @@ import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.nicehttp.RequestBodyTypes
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
